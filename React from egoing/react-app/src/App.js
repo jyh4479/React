3ce6887ccebe69d34f1c165fee90c118cc5ebd35
Jeong yong hoon <jyh.dev@gmail.com>
2021-04-06 16:21:02 +0900

    Update: styd record
    생활코딩 react ~16챕터까지

--- React from egoing/react-app/src/App.js
+++ React from egoing/react-app/src/App.js
@@ -36,7 +36,9 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      subject:{title:"WEB", sub:"World Wid Web!"},
+      mode:"read",
+      subject:{title:"WEB", desc:"World Wid Web!"},
+      welcome:{title:"Welcome!", desc:"This is welcome mode!"},
 
       contents:[
         {id:1, title:'test1', desc: 'This is test1'},
@@ -47,9 +49,35 @@ class App extends Component{
   }
 
   render(){
+    var _title, _desc=null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }
+    else if(this.state.mode==='read'){
+      _title=this.state.subject.title;
+      _desc=this.state.subject.desc;
+    }
     return(
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <h1><a href='/' onClick={function(e){
+          console.log(e);
+          alert(123);
+          e.preventDefault() //--> a태그의 리로드하는 기본 방식을 막아줌
+          //this.state.mode='welcome'; --> 1. event function 안에서 this는 아무것도 가리키고있지 않다.(bind(this)로 해결가능)
+          //                           --> 2. 리액트의 변수불변 법칙? --> set함수를 이용해야함
+          if(this.state.mode==="read"){
+            this.setState({
+              mode:'welcome'
+            });
+          }
+          else{
+            this.setState({
+              mode:'read'
+            });
+          }
+        }.bind(this)}>MODE</a></h1>
+        <Subject title={_title} sub={_desc}></Subject>
         <ComponentTest data={this.state.contents}></ComponentTest>
       </div>
     );
--- React from egoing/readme.txt
+++ React from egoing/readme.txt
@@ -10,4 +10,10 @@
 
 6. Constructor를 사용해서 render시작 전 data를 정의할 수 있다.
 
-7. Data를 props로 받고 state를 변경하는 방법 --> list를 출력할때 react자체에서 각 원소의 key값을 요구함(안넣었을 경우   브라우저 관리자도구 콘솔에서 오류 확인가능)
+7. Data를 props로 받고 state를 변경하는 방법 --> list를 출력할때 react자체에서 각 원소의 key값을 요구함(안넣었을 경우 브라우저 관리자도구 콘솔에서 오류 확인가능)
+
+8. a태그의 reload하는 특징을 막기위해 function에 e인자를 받고 e.preventDefault를 통해서 태그 속성의 기본 동작을 막을 수 있다.
+
+9. 이벤트가 발생할때 함수를 선언하여 Component의 mode를 바꾸면서 출력되는 내용을 변경할 수 있다 --> 그 과정에서 bind함수에 대한 학습 필요
+
+10. React에서 state값을 변경하려면 setState라는 함수를 사용하여 값을 변경해야함
