4eb689c77b87e4d14cd426ce6ab5a40011f610b8
Jeong yong hoon <jyh.dev@gmail.com>
2021-04-30 17:30:41 +0900

    Study record of React
    props를 통한 css변경을 실습함 --> hover같은 style은 props로 어떻게 변경할 수 있을까? (조건부로 나누는 방법은 효율적이지 못할거같음)

--- redux-my-react-app/src/Components/TopMenu/TopMenu.js
+++ redux-my-react-app/src/Components/TopMenu/TopMenu.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import './TopMenu.css';
 
+import eventReducer from "../../redux/reducers/eventReducer";
+import action from "../../redux/actions";
+import {connect} from 'react-redux';
+import { toJS } from 'immutable'
+
 class TopMenu extends Component{
+  constructor(props){
+    super(props);
+    this.state={}
+  }
 
   render(){
+
+    const reduxColor = this.props.eventReducerState.color || '교육'
+    console.log(reduxColor.color)
+
+    const colorStyle={
+      backgroundColor: reduxColor.color
+    }
+
     var titleList=this.props.dataList;
 
     return(
@@ -25,4 +42,17 @@ class TopMenu extends Component{
     )
   }
 }
-export default TopMenu;
+const fromJsStateToProps = (state) => {
+    const eventReducer = state.eventReducer;
+    return {
+        eventReducerState: eventReducer
+    };
+}
+
+const fromJsDispatchToProps = dispatch => ({
+    // onChangeText: () => dispatch(action.eventAction.setText({text:'교육끝'})),
+    // onChangeText2: () => dispatch(action.eventAction.setText({text:'교육'})),
+});
+
+
+export default connect(fromJsStateToProps, fromJsDispatchToProps)(TopMenu);
