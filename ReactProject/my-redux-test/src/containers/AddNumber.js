90d3cabd6c2fae117864c6845fc49788e6fcf5b5
Jeong yong hoon <jyh.dev@gmail.com>
2021-05-03 14:48:51 +0900

    Study record of React
    1. react-redux를 사용하는 이유
    
    2. 종속성을 없애면서 container가 생기기 때문에 작업량이 많아진다 그리고 props를 넘기려고해도 container를 한번 거쳐서 오기 때문에 수정한 코드와 마찬가지로 react-redux를 사용하면 좋다

--- ReactProject/my-redux-test/src/containers/AddNumber.js
+++ ReactProject/my-redux-test/src/containers/AddNumber.js
@@ -1,13 +1,24 @@
 import AddNumber from "../components/AddNumber";
-import React,{Component} from "react";
-import store from "../store";
+import {connect} from "react-redux";
 
-export default class extends Component{
-    render(){
-        return(
-            <AddNumber onClick={function (size){
-                store.dispatch({type:'INCREMENT',size:size});
-            }.bind(this)}></AddNumber>
-        )
+function mapDispatchToProps(dispatch){
+    return{
+        onClick:function(size){
+            dispatch({type:'INCREMENT', size:size});
+        }
     }
 }
+export default connect(null, mapDispatchToProps)(AddNumber);
+// import AddNumber from "../components/AddNumber";
+// import React,{Component} from "react";
+// import store from "../store";
+//
+// export default class extends Component{
+//     render(){
+//         return(
+//             <AddNumber onClick={function (size){
+//                 store.dispatch({type:'INCREMENT',size:size});
+//             }.bind(this)}></AddNumber>
+//         )
+//     }
+// }
--- ReactProject/my-redux-test/src/index.js
+++ ReactProject/my-redux-test/src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/* redux 설정 */
+// /* redux 설정 */ --> 책임님이랑 작성한 코드
+// import{Provider} from "react-redux";
+// import{combineReducers,createStore} from "redux";
+// import reducers from './redux/reducers';
+//
+// const store = createStore(combineReducers(reducers),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>
+//   ,
+//   document.getElementById('root')
+// );
+
+import store from './store';
 import{Provider} from "react-redux";
-import{combineReducers,createStore} from "redux";
-import reducers from './redux/reducers';
-
-const store = createStore(combineReducers(reducers),window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__());
-
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
