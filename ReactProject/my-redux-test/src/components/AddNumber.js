e7416f79c032fdb1be48aa74f5987274d61e89d5
Jeong yong hoon <jyh.dev@gmail.com>
2021-05-03 13:54:45 +0900

    Study record of React
    1. Redux의 종속성을 제거하는 작업
    
    2. Store를 Component에서 바로 사용하면 Store에 의지하며 재사용하기 힘든 문제점이 발생 --> (1)컨테이너 Component와 (2)프레젠테이셔널 Component로 나누어 (1)에서는 Redux에 대한 처리를 하고 (2)에서는 View를 담당하며 종속성을 제거하고 재사용성을 높일 수 있음

--- ReactProject/my-redux-test/src/components/AddNumber.js
+++ ReactProject/my-redux-test/src/components/AddNumber.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import store from "../store";
 
 class AddNumber extends Component{
     state={size:1}
@@ -9,8 +8,9 @@ class AddNumber extends Component{
                 <h1>Add Number</h1>
                 <input type="button"value="+" onClick={function(){
 
-                    {/*this.props.onClick(this.state.size);*/}
-                    store.dispatch({type:'INCREMENT', size:this.state.size})
+                    this.props.onClick(this.state.size);
+                    // store.dispatch({type:'INCREMENT', size:this.state.size}) //종속성을 제거하면서 store의 의존성을
+                    // container component에게 맡김 --> 온전히 view에만 집중할 수 있다.
 
                 }.bind(this)}
                 ></input>
