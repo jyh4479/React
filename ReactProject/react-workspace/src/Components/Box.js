f2750a7f05ed35c6c24fce741d72b2090e7532b1
Jeong yong hoon <jyh.dev@gmail.com>
2021-08-12 22:57:00 +0900

    Scss Test

--- ReactProject/react-workspace/src/Components/Box.js
+++ ReactProject/react-workspace/src/Components/Box.js
@@ -4,9 +4,9 @@ import './Box.scss'
 const Box = () => {
     return (
         <div className={'wrapper'}>
-            <div className={'box red'}></div>
-            <div className={'box blue'}></div>
-            <div className={'box green'}></div>
+            <div className={'box-green'}></div>
+            <div className={'box-blue'}></div>
+            <div className={'box-red'}></div>
         </div>
     )
 }
