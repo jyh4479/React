befa56b91bbb0b345954d4c7ddd9426fccbfcf68
Jeong yong hoon <jyh.dev@gmail.com>
2022-03-13 01:16:04 +0900

    DropDown Menu 컴포넌트 구현
    직접 작성한 순수 드롭다운 메뉴 구현
    다음으로 라이브러리 써보고 코드 최적화하기
    + 돔 조작 관련 동작원리 학습

--- muicomponent/src/components/dropdown/index.js
+++ muicomponent/src/components/dropdown/index.js
@@ -1,9 +1,87 @@
-import React from 'react';
+import React, {useRef} from "react";
+import "./dropdown.css"
+import {AddBoxRounded} from "@mui/icons-material";
 
-const Dropdown = () => {
-    return (
-        <div>HI</div>
-    )
+
+const Dropdown = props => {
+    // 직접 돔을 조작하는 아래 로직
+    // let subMenuList
+    // useEffect(() => {
+    //     subMenuList = document.querySelectorAll(".sub-menu")
+    // }, [])
+
+    // 돔을 조작할때 리액트에서는 Ref 사용을 권고하는데 동작 원리와 이유 찾아보기
+    const subMenuList = useRef([]);
+
+    const open = key => {
+        const subMenu = subMenuList.current[key]
+        const childCount = subMenu.childElementCount
+
+        if (subMenu.classList.contains("hide")) {
+            subMenu.style.maxHeight = `${childCount * 50}px`
+            subMenu.classList.remove("hide")
+            subMenu.classList.add("show")
+        } else if (subMenu.classList.contains("show")) {
+            subMenu.style.maxHeight = `0px`
+            subMenu.classList.remove("show")
+            subMenu.classList.add("hide")
+        }
+    }
+
+    return (<div className={"container"}>
+        <ul className={"menu"}>
+            <li>
+                <div>
+                    Menu1
+                    <AddBoxRounded onClick={() => open(0)}/>
+                </div>
+                <ul ref={el => subMenuList.current[0] = el} className={"sub-menu hide"}>
+                    <li>
+                        <div>Sub1</div>
+                    </li>
+                    <li>
+                        <div>Sub2</div>
+                    </li>
+                    <li>
+                        <div>Sub3</div>
+                    </li>
+                </ul>
+            </li>
+
+
+            <li>
+                <div>Menu2
+                    <AddBoxRounded onClick={() => open(1)}/>
+                </div>
+                <ul ref={el => subMenuList.current[1] = el} className={"sub-menu hide"}>
+                    <li>
+                        <div>Sub1</div>
+                    </li>
+                    <li>
+                        <div>Sub2</div>
+                    </li>
+                </ul>
+            </li>
+
+
+            <li>
+                <div>Menu3
+                    <AddBoxRounded onClick={() => open(2)}/>
+                </div>
+                <ul ref={el => subMenuList.current[2] = el} className={"sub-menu hide"}>
+                    <li>
+                        <div>Sub1</div>
+                    </li>
+                    <li>
+                        <div>Sub2</div>
+                    </li>
+                    <li>
+                        <div>Sub3</div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>)
 }
 
 export default Dropdown;
